bd6b8bb25d74b5ad41bb468318a1aa241c29601c
Dino-Martinez <>
2021-02-09 00:31:06 -0800

    users can log in

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,10 @@ router.get('/sign-up', (req, res) => {
   return res.render('sign-up')
 })
 
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
 router.post('/sign-up', (req, res) => {
   // Create User
   const user = new User(req.body)
@@ -25,9 +29,61 @@ router.post('/sign-up', (req, res) => {
     })
 })
 
+router.post('/login', (req, res) => {
+  const username = req.body.username
+  const password = req.body.password
+  // Find this user name
+  User.findOne({ username }, 'username password')
+    .then((user) => {
+      if (!user) {
+        // User not found
+        return res.status(401).send({ message: 'Wrong username or password' })
+      }
+      // Check the password
+      user.comparePassword(password, (err, isMatch) => {
+        if (!isMatch) {
+          // Password does not match
+          return res.status(401).send({ message: 'Wrong username or password' })
+        }
+
+        // Create a token
+        const token = jwt.sign(
+          { _id: user._id, username: user.username },
+          process.env.SECRET,
+          {
+            expiresIn: '60 days',
+          }
+        )
+        // Set a cookie and redirect to root
+        res.cookie('nToken', token, { maxAge: 900000, httpOnly: true })
+        res.redirect('/')
+      })
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
+
 router.get('/logout', (req, res) => {
   res.clearCookie('nToken')
   res.redirect('/')
+})
+
+router.get('/drop-all', (req, res) => {
+  User.deleteMany({ username: 'username' }).then(() => {
+    res.redirect('/')
+  })
+})
+
+router.get('/all', (req, res) => {
+  User.find({})
+    .lean()
+    .then((users) => {
+      res.send(users)
+    })
+    .catch((err) => {
+      console.log(err.message)
+    })
 })
 
 module.exports = router
